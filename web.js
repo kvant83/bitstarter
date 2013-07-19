e0aa8c5cbb92d7ce7bc7b2b2244d6b733676b4f6
Alexander Larionov <kvant@mail.com>
2013-07-19 12:30:40 +0000

    Use line from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
-var express = require('express');
+press = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
+var buf = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('ascii'));
 });
 
 var port = process.env.PORT || 5000;
